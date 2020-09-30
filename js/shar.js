82ecd26b950804520335510599781dd5fea29bb8
Sharavanan Balasundaravel <43992706+sharavananpa@users.noreply.github.com>
2020-09-30 15:49:10 +0530

    Add music and responsiveness

--- js/shar.js
+++ js/shar.js
@@ -1,20 +1,21 @@
 function showContent(id) {
     document.getElementById('main-content').innerHTML = document.getElementById(id).innerHTML;
+    console.log("Content changed!");
 }
 
-var color = 0;
+var color = 1;
+var prev = 1;
 
 function changeColor() {
-    document.body.classList.replace(document.body.classList[0], "theme-" + ++color);
+    document.body.classList.remove("theme-" + prev);
+    document.body.classList.add("theme-" + ++color);
+    console.log("Color changed!");
+    prev = color;
     if(color==8)
         color = 0;
 }
 
 function darkMode() {
-    if(document.body.classList[1] != "dark") {
-        document.body.classList.add('dark');
-    }
-    else {
-        document.body.classList.remove('dark');
-    }
+    document.body.classList.toggle('dark');
+    console.log("Toggle dark mode!");
 }
